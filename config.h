dcd6a5057c7f78c9ffca1eed63fd10ef8d6c1ec7
Chris Rendle-Short <chris@killred.net>
2016-06-05 20:47:27 +1000

    Shrink font again.

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]            = "-*-fixed-medium-r-normal-*-14-*-*-*-*-*-*-*";
+static const char font[]            = "-*-fixed-medium-r-normal-*-13-*-*-*-*-*-*-*";
 static const char normbordercolor[] = "#2e3436";
 static const char normbgcolor[]     = "#222222";
 static const char normfgcolor[]     = "#999999";
